144b013ecb3a1d59d4e5320bd59e30791a0475b8
Levchuk Misha <mykhaylo.levchuk@ukr.net>
2020-03-03 20:06:21 +0100

    Done: AppDependency Container + RootViewController

---
+++ Dota2App/AppDependencyContainer/AppDependencyContainer.swift
@@ -0,0 +1,13 @@
+//
+//  AppDependencyContainer.swift
+//  Dota2App
+//
+//  Created by Levchuk Misha on 03/03/2020.
+//  Copyright © 2020 Levchuk Misha. All rights reserved.
+//
+
+import Foundation
+
+class AppDependencyContainer {
+    
+}
---
+++ Dota2App/Screens/SearchAccount/SearchAccountViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Dota2App
+//
+//  Created by Levchuk Misha on 03/03/2020.
+//  Copyright © 2020 Levchuk Misha. All rights reserved.
+//
+
+import UIKit
+
+class SearchAccountViewController: UIViewController {
+
+    let viewModel: SearchAccountViewModel
+
+    init(viewModel: SearchAccountViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
+
