f14d23a986b0aa0c5f009cf81f21858700e971f5
Levchuk Misha <mykhaylo.levchuk@ukr.net>
2020-03-03 19:17:00 +0100

    initial commit

---
+++ Dota2App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dota2App
+//
+//  Created by Levchuk Misha on 03/03/2020.
+//  Copyright © 2020 Levchuk Misha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
