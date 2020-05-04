a80b6b65bbbcd734882e6c8773a5915ceed32584
developer <mykhaylo.levchuk@hemmersbach.com>
2020-05-04 09:31:35 +0200

    Rebuilt facade + presenting errors to user

---
+++ Dota2App/AppDependencyContainer/Factories/AppDependencyContainer+HeroesRepositoryFactory.swift
@@ -0,0 +1,9 @@
+//
+//  AppDependencyContainer+HeroesRepositoryFactory.swift
+//  Dota2App
+//
+//  Created by Mykhaylo Levchuk on 20/04/2020.
+//  Copyright © 2020 Levchuk Misha. All rights reserved.
+//
+
+import Foundation
---
+++ Dota2App/AppDependencyContainer/Factories/AppDependencyContainer+HeroesServiceFactory.swift
@@ -0,0 +1,9 @@
+//
+//  AppDependencyContainer+HeroesServiceFactory.swift
+//  Dota2App
+//
+//  Created by Mykhaylo Levchuk on 17/04/2020.
+//  Copyright © 2020 Levchuk Misha. All rights reserved.
+//
+
+import Foundation
